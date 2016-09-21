8bd44ba712766f2cd788ebefe699d32b10dc861c
marcosunic <marcos_a_dias@estudante.sc.senai.br>
2016-09-21 20:09:09 +0000

    dados_pessoais

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+
   root 'home#index'
+
+  get 'home/dados_pessoais'
+
   get 'auth/:provider/callback', to: 'sessions#create'
+
   delete 'sign_out', to: 'sessions#destroy', as:'sign_out'
 
   # The priority is based upon order of creation: first created -> highest priority.
